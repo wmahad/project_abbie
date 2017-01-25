9d4624e59207cc1e4ed4c5663e78b61e32c155e3
Mahad Walusimbi <engmk123@gmail.com>
2017-01-26 02:35:44 +0300

    Set up static assets and file serving

--- project_abbie/settings.py
+++ project_abbie/settings.py
@@ -122,7 +122,17 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Simplified static file serving
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
